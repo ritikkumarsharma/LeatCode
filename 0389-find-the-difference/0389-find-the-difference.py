b9e64ca691b096fc99c3d309af82a37c3745963b
Ritik Sharma <126388687+ritikkumarsharma@users.noreply.github.com>
2025-01-17 22:42:53 +0530

    Time: 3 ms (53.89%), Space: 17.8 MB (36.17%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        count = {}
+        for c in t:
+            count [c] = count.get(c, 0) + 1
+        for c in s:
+            count[c] -= 1
+            if count[c] == 0:
+                if count[c] == 0:
+                    del count[c]
+        return list(count.keys())[0] 
